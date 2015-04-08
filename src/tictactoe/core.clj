6024d1195528fdd359f552c4fe7a60f00d59595d
Ross Davidson <ross.e.davidson@gmail.com>
2015-04-08 14:05:49 -0500

    Error checking on playing n games argument

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -3,8 +3,17 @@
   (:require [tictactoe.tictactoe :refer [play-game user-input play-n-games]]
             [tictactoe.print :refer [print-score print-summary]]))
 
+(defn parse-n-games-arg
+  [num-games-str]
+  (try
+    (Integer/parseInt num-games-str)
+    (catch Exception e "Please enter a positive integer for number of games.")))
+
 (defn -main
   [& args]
   (if (nil? args)
     (print-score (play-game user-input))
-    (print-summary (play-n-games (Integer/parseInt (nth args 0))))))
+    (let [num-games (parse-n-games-arg (nth args 0))]
+      (if (integer? num-games)
+        (print-summary (play-n-games num-games))
+        (println num-games)))))
